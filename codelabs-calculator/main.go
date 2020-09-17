276c8301d2ccc93318cde561a300c3889ff25bfc
fabianotessarolo <fabiano.tessarolo@gmail.com>
2020-09-17 21:59:15 +0000

    WIP: Session 2

--- codelabs-calculator/main.go
+++ codelabs-calculator/main.go
@@ -7,29 +7,37 @@ import (
 	_ "log"
 )
 
-func Sum(a int64, b int64) (int64, error) {
-	myReturn := (a + b)
+func sum(a int64, b int64) (myReturn int64, err error) {
+	myReturn = (a + b)
 	return myReturn, nil
 }
 
-func Divide(a int64, b int64) (myReturn int64, err error) {
+func divide(a int64, b int64) (myReturn int64, err error) {
 	err = nil
 	if b == 0 {
 		err = errors.New("Cannot divide by zero")
-		return myReturn,err
+		return myReturn, err
 	}
 
 	myReturn = a / b
 	return myReturn, err
 }
 
-func main() {
+func subtract(a int64, b int64) (myReturn int64, err error) {
+	myReturn = (a - b)
+	return myReturn, nil
+}
 
-	
+func multiply(a int64, b int64) (myReturn int64, err error) {
+	myReturn = (a * b)
+	return myReturn, nil
+}
+
+func main() {
 	var myVariable int64 = 6
-	myReturn,_ := Sum(myVariable, myVariable)
+	myReturn, _ := sum(myVariable, myVariable)
 	fmt.Println(myReturn)
-	myReturn, _ = Divide(myVariable, 0)
+	myReturn, _ = divide(myVariable, 0)
 	fmt.Println(myReturn)
 }
 
