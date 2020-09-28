65a4705b613fcd40c5280184783157c8d2912925
fabianotessarolo <fabiano.tessarolo@gmail.com>
2020-09-28 22:09:32 +0000

    Session 3

---
+++ codelabs-calculator/calc/calculator.go
@@ -0,0 +1,66 @@
+package calc
+
+type calculator struct {
+	input int64
+	output int64
+	operator func()
+}
+
+type operation interface {
+	Calculate() (int64, error)
+}
+
+
+type SumType calculator
+type MultiplyType calculator
+type DivideType calculator
+type SubtractType calculator
+
+
+
+func (s *SumType) Calculate() (int64, error) {
+	var err error 
+	s.output, err = Sum(s.input, s.input)
+	if err != nil {
+		return s.output, err
+	}
+	return s.output, nil
+}
+
+func (s *MultiplyType) Calculate() (int64, error) {
+	var err error 
+	s.output, err = Multiply(s.input, s.input)
+	if err != nil {
+		return s.output, err
+	}
+	return s.output, nil
+}
+
+func (s *DivideType) Calculate() (int64, error) {
+	var err error 
+	s.output, err = Divide(s.input, s.input)
+	if err != nil {
+		return s.output, err
+	}
+	return s.output, nil
+}
+
+func (s *SubtractType) Calculate() (int64, error) {
+	var err error 
+	s.output, err = Subtract(s.input, s.input)
+	if err != nil {
+		return s.output, err
+	}
+	return s.output, nil
+}
+
+func CheckOperation(op string) (operation, error) {
+	operations := map[string]operation{
+		"+": &SumType{input: 2},
+		"-": &SubtractType{input: 2},
+		"/": &DivideType{input: 2},
+		"*": &MultiplyType{input: 2},
+	}
+	v := operations[op] 
+	return v,nil
+}
--- codelabs-calculator/cmd/calculator.go
+++ codelabs-calculator/cmd/calculator.go
@@ -4,19 +4,50 @@ package cmd
 import (
 	"calculator/calc"
 	"os"
-	"strconv"
 	"fmt"
 )
 
 // Execute The function execute ...
 func Execute() {
 
-	primeironumero, _ := strconv.ParseInt(os.Args[1], 10, 64)
-	segundonumero, _ := strconv.ParseInt(os.Args[2], 10, 64)
-
+	// primeironumero, _ := strconv.ParseInt(os.Args[1], 10, 64)
+	// segundonumero, _ := strconv.ParseInt(os.Args[2], 10, 64)
 	// var myVariable int64 = 6
-	myReturn, _ := calc.Sum(primeironumero,segundonumero)
-	 fmt.Println(myReturn)
+	// myReturn, _ := calc.Sum(primeironumero,segundonumero)
+	//  fmt.Println(myReturn)
 	// myReturn, _ = calc.Divide(myVariable, 0)
 	// fmt.Println(myReturn)
+
+	o, err := calc.CheckOperation("+")
+	if err != nil {
+		os.Exit(127)
+	}
+
+	r, _ := o.Calculate()
+	fmt.Println(r)
+
+
+	o, err = calc.CheckOperation("-")
+	if err != nil {
+		os.Exit(127)
+	}
+
+	r, _ = o.Calculate()
+	fmt.Println(r)
+
+	o, err = calc.CheckOperation("*")
+	if err != nil {
+		os.Exit(127)
+	}
+
+	r, _ = o.Calculate()
+	fmt.Println(r)
+
+	o, err = calc.CheckOperation("/")
+	if err != nil {
+		os.Exit(127)
+	}
+
+	r, _ = o.Calculate()
+	fmt.Println(r)
 }
